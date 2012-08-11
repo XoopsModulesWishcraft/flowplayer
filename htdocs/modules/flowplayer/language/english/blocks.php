8ce1fdbe42950de01d4c9b0fabd83929168d1b4a
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- htdocs/modules/flowplayer/language/english/blocks.php
+++ htdocs/modules/flowplayer/language/english/blocks.php
@@ -1,8 +1,8 @@
 <?php
 
-	define('_BL_FLOWPLAYER_BLOCK_FID', 'Video to Play:&nbsp;');
-	define('_BL_FLOWPLAYER_BLOCK_WIDTH', 'Width:&nbsp;');
-	define('_BL_FLOWPLAYER_BLOCK_HEIGHT', 'Height:&nbsp;');
-	define('_BL_FLOWPLAYER_BLOCK_REFERENCE', 'Reference ID:&nbsp;');
+	define('_BL_FLOWPLAYER_BLOCK_FID','Video to Play:&nbsp;');
+	define('_BL_FLOWPLAYER_BLOCK_WIDTH','Width:&nbsp;');
+	define('_BL_FLOWPLAYER_BLOCK_HEIGHT','Height:&nbsp;');
+	define('_BL_FLOWPLAYER_BLOCK_REFERENCE','Reference ID:&nbsp;');
 	
 ?>
